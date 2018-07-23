93a26496391167af14a8c848ed377e7395290e74
Heather Foxhill <HeatherRFoxhill@gmail.com>
2018-07-23 14:47:02 -0500

    moved json and server.js

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 // request data from various URLs. 
 // ================================================================================
 
-require('./routing/apiRoutes.js')(app); 
-require('./routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
 
 // ==============================================================================
